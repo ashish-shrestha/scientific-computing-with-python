18271a5922dabb191578e324338a3d1db17f1e1d
Ashish Shrestha <ashish@Ashishs-MacBook-Air.local>
2022-11-12 09:37:29 -0500

    fixed am/pm determination bug and finished project

--- time_calculator.py
+++ time_calculator.py
@@ -40,98 +40,97 @@ def add_time(start, duration, day =''):
     final_hours = hours + hours_to_add
     if final_hours > 12:
         final_hours = final_hours % 12
+        if final_hours == 0:
+            final_hours = 12
 
     # print('final hours:', final_hours)
 
     # determine distance to 12:00 to determine am/pm
-    minutes_to_noon = (12 * 60) - ((hours * 60) + minutes)
-    # print('minutes to 12:', minutes_to_noon)
-    total_minutes_to_add = (hours_to_add * 60) + minutes_to_add
+    minutes_to_twelve = (12 * 60) - ((hours * 60) + minutes)
+    # print('minutes to 12:', minutes_to_twelve)
+    total_minutes_to_add = (hours_to_add * 60) + final_minutes
     # print('total minutes to add:', total_minutes_to_add)
-    if total_minutes_to_add - minutes_to_noon >= 0:
-        print('total minutes to add is beyond noon time')
+    if total_minutes_to_add - minutes_to_twelve >= 0:
+        # print('total minutes to add is beyond 12 time')
         if initial_am_pm == 'PM':
             final_am_pm = 'AM'
+
+            if (total_minutes_to_add - minutes_to_twelve) >= (12 * 60):
+                cycles = (total_minutes_to_add - minutes_to_twelve) // (12 * 60)
+                if cycles % 2 != 0:
+                    if final_am_pm == 'PM':
+                        final_am_pm = 'AM'
+                    else:
+                        final_am_pm = 'PM'
         else:
             final_am_pm = 'PM'
-        remaining_minutes = total_minutes_to_add - minutes_to_noon
-        print('remaining minutes', remaining_minutes)
+            if (total_minutes_to_add - minutes_to_twelve) >= (12 * 60):
+                cycles = (total_minutes_to_add - minutes_to_twelve) // (12 * 60)
+                if cycles % 2 != 0:
+                    if final_am_pm == 'PM':
+                        final_am_pm = 'AM'
+                    else:
+                        final_am_pm = 'PM'
+        '''    
+        remaining_minutes = total_minutes_to_add - minutes_to_twelve
+        # print('remaining minutes', remaining_minutes)
         if remaining_minutes >= (12 * 60):
             result = remaining_minutes // (12 * 60)
-            print('result:', result)
-            if result % 2 == 0:
+            # print('result:', result)
+            if result % 2 != 0:
                 if final_am_pm == 'AM':
                     final_am_pm = 'PM'
                 else:
                     final_am_pm = 'PM'
-            else:
-                final_am_pm = initial_am_pm
         '''
-        else:
-            if initial_am_pm == 'AM':
-                final_am_pm = 'PM'
-            else:
-                final_am_pm = 'AM'
-        '''
-    '''
-    else:
-        if initial_am_pm == 'AM':
-            final_am_pm = 'PM'
-        else:
-            final_am_pm = 'PM'
-            
-    '''
 
     # print('final ampm:', final_am_pm)
     result = str(final_hours) + ":" + final_minutes_formatted + " " + final_am_pm
 
     # determining days passed
-    # if day != '':
     if initial_am_pm == 'AM':
-        minutes_to_midnight = (12 * 60) + minutes_to_noon
+        minutes_to_midnight = (12 * 60) + minutes_to_twelve
     else:
-        minutes_to_midnight = minutes_to_noon
+        minutes_to_midnight = minutes_to_twelve
 
+    days_passed = 0
     if total_minutes_to_add >= minutes_to_midnight:
-        '''        
-        print('total minutes to add:', total_minutes_to_add)
-        print('minutes to midnight:', minutes_to_midnight)
-        '''
+        # print('total minutes to add:', total_minutes_to_add)
+        # print('minutes to midnight:', minutes_to_midnight)
+        days_passed += 1
         remaining_minutes_to_add = total_minutes_to_add - minutes_to_midnight
         # print('remaining_minutes to add:', remaining_minutes_to_add)
         if day != '':
             final_day_num = days.index(day) + 1
         else:
             final_day_num = 1
-        # print('final dayNum:', final_dayNum)
+        # print('final dayNum:', final_day_num)
         final_day_num += remaining_minutes_to_add // (24 * 60)
-        total_days_passed = final_day_num
+        days_passed += remaining_minutes_to_add // (24 * 60)
         if final_day_num > 7:
-                final_day_num = (final_day_num % 7)
-        # print('final day is:', days[final_dayNum])
+            final_day_num = (final_day_num % 7)
+        # print('final day is:', days[final_day_num])
         final_day = days[final_day_num]
         if day != '':
             result = result + ', ' + final_day
-        if total_days_passed != 0:
+        if days_passed != 0:
             if day != '':
-                if total_days_passed == 1:
+                if days_passed == 1:
                     result = result + ' (next day)'
                 else:
-                    result = result + ' (' + str(total_days_passed) + ' days later)'
+                    result = result + ' (' + str(days_passed) + ' days later)'
             else:
-                if total_days_passed == 1:
+                if days_passed == 1:
                     result = result + ' (next day)'
                 else:
-                    result = result + ' (' + str(total_days_passed) + ' days later)'
+                    result = result + ' (' + str(days_passed) + ' days later)'
     else:
         if day != '':
             final_day = day
-            print('final day is:', day)
+            # print('final day is:', day)
             result = result + ', ' + final_day
+    return result
 
-    print(result)
-
-
-add_time("11:43 PM", "24:20", "tueSday")
+add_time("2:59 PM", "24:00")
 
     # return new_time
