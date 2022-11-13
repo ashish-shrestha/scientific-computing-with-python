9fbdc9384975c0fa3a4f14de00fb6854466902d4
Ashish Shrestha <ashish@Ashishs-MacBook-Air.local>
2022-11-12 20:19:40 -0500

    creating the functions with the class category

--- budget_app.py
+++ budget_app.py
@@ -2,6 +2,12 @@ class Category:
     def __init__(self, ledger):
         ledger = []
 
+    def get_balance(self):
+        balance = 0
+        for i in self.ledger:
+            balance += i["amount"]
+        return balance
+
     def deposit(self, amount, description=''):
         amount = amount
         description = description
@@ -9,24 +15,30 @@ class Category:
         self.ledger.append(entry)
 
     def withdraw(self, amount, description=''):
-        amount = -1*amount
+        amount = amount
+        current_balance = self.get_balance()
+        if self.check_funds():
+            entry = {"amount": -1 * amount, "description": description}
+            return True
+        else:
+            return False
 
-        # how to determine what balance is?
+    def transfer(self, amount, category):
+        amount = amount
+        category = category
+        if self.check_funds():
+            self.withdraw(amount, 'Transfer to ' + category)
+            category.deposit(amount, 'Transfer from ' + self)
+            return True
+        else:
+            return False
 
-    def get_balance(self):
-        balance = 0
-        for i in self.ledger:
-            balance += i["amount"]
-
-        return balance
-
-    
-
-
-
-
-
-
-
+    def check_funds(self, amount):
+        if self.get_balance() < amount:
+            return False
+        else:
+            return True
 
 def create_spend_chart(categories):
+    categories = categories
+    
